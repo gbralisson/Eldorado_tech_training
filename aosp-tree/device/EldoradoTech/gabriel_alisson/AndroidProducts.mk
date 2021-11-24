f585646acfdb0f982ed6e4c3a8c2a041f881317f
Ricardo Santos <jigos@pop-os.localdomain>
2021-11-24 18:54:38 -0300

    aosp-tree add

---
+++ aosp-tree/device/EldoradoTech/gabriel_alisson/AndroidProducts.mk
@@ -0,0 +1,8 @@
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/gabriel.mk
+
+COMMON_LUNCH_CHOICES := \
+	gabriel-eng \
+	gabriel-userdebug    
+
+
---
+++ aosp-tree/device/generic/goldfish/questao3/src/com/example/helloaosp/BroadcastListener.java
@@ -0,0 +1,9 @@
+package com.example.helloaosp;
+
+public interface BroadcastListener {
+	public void setFullAnimation();
+	public void setHighAnimation();
+	public void setMiddleAnimation();
+	public void setLowAnimation();
+	public void setEmptyAnimation();
+}
