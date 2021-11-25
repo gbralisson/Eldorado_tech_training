314e8df6a99df5a28870a072329f737dc1ecbcb3
Ricardo Santos <jigos@pop-os.localdomain>
2021-11-25 08:09:57 -0300

    add questao4

---
+++ questao4/app/SampServiceApp/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := $(call all-java-files-under,src)
+LOCAL_REQUIRED_MODULES := com.android.sampservice 
+LOCAL_JAVA_LIBRARIES := com.android.sampservice \
+	framework
+LOCAL_PACKAGE_NAME := SampServiceApp
+LOCAL_SDK_VERSION := current
+LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+include $(BUILD_PACKAGE)
